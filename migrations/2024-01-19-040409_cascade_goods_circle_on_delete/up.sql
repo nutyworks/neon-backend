53128243784f9360b36b9932c8971417a9c03857
Beomjun Kim <nutyworks@gmail.com>
2024-01-19 13:23:05 +0900

    Cascade goods-bundle association on goods delete

--- migrations/2024-01-19-040409_cascade_goods_circle_on_delete/up.sql
+++ migrations/2024-01-19-040409_cascade_goods_circle_on_delete/up.sql
@@ -3,3 +3,8 @@ ALTER TABLE circle_goods
 DROP CONSTRAINT IF EXISTS circle_goods_goods_id_fkey,
 ADD CONSTRAINT circle_goods_goods_id_fkey
 FOREIGN KEY (goods_id) REFERENCES goods(id) ON DELETE CASCADE;
+
+ALTER TABLE goods_in_bundle
+DROP CONSTRAINT IF EXISTS goods_in_bundle_goods_id_fkey,
+ADD CONSTRAINT goods_in_bundle_goods_id_fkey
+FOREIGN KEY (goods_id) REFERENCES goods(id) ON DELETE CASCADE;
